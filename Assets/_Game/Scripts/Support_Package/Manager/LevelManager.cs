6bf7607cb0785be7aa216cdc42faeec9c8fb9e6c
Nguyễn Tạ Quốc Cường <71555634+fairchild7@users.noreply.github.com>
2023-12-20 14:38:08 +0700

    add support package

---
+++ Assets/_Game/Scripts/Support_Package/Manager/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    //[SerializeField] Level[] levels;
+    //public Level currentLevel;
+
+    public void Start()
+    {
+        OnLoadLevel(0);
+        OnInit();
+    }
+
+    //khoi tao trang thai bat dau game
+    public void OnInit()
+    {
+        //player.OnInit();
+    }
+
+    //reset trang thai khi ket thuc game
+    public void OnReset()
+    {
+        
+    }
+
+    //tao prefab level moi
+    public void OnLoadLevel(int level)
+    {
+        //if (currentLevel != null)
+        //{
+        //    Destroy(currentLevel.gameObject);
+        //}
+
+        //currentLevel = Instantiate(levels[level]);
+    }
+
+}
